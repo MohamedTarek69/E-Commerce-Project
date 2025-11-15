03dc6f0c4d0d252d20d62f4aac1461b81bd3f0e6
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-11-15 18:43:20 +0200

    Api Session 02 & 03

---
+++ E Commerce.Services/Services/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using E_Commerce.Domain.Contracts;
+using E_Commerce.Domain.Entities.ProductModule;
+using E_Commerce.Services_Abstraction.Interfaces;
+using E_Commerce.Shared.DTOs.ProductDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Services.Services
+{
+    public class ProductService : IProductServices
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ProductService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork=unitOfWork;
+            _mapper=mapper;
+        }
+
+        public async Task<IEnumerable<BrandDTO>> GetAllBrandsAsync()
+        {
+            var Brands = await _unitOfWork.GetRepository<ProductBrand,int>().GetAllAsync();
+            // ProductBrand - BrandDTO {Automatic Mapping}
+            return _mapper.Map<IEnumerable<BrandDTO>>(Brands);
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetAllProductsAsync()
+        {
+            var products =  await _unitOfWork.GetRepository<Product,int>().GetAllAsync();
+            return _mapper.Map<IEnumerable<ProductDTO>>(products);
+        }
+
+        public async Task<IEnumerable<TypeDTO>> GetAllTypesAsync()
+        {
+            var Types = await _unitOfWork.GetRepository<ProductType, int>().GetAllAsync();
+            return _mapper.Map<IEnumerable<TypeDTO>>(Types);
+        }
+
+        public async Task<ProductDTO?> GetProductByIdAsync(int id)
+        {
+            var product = await _unitOfWork.GetRepository<Product, int>().GetByIdAsync(id);
+            return _mapper.Map<ProductDTO>(product);
+        }
+    }
+}
---
+++ E Commerce.Web/Extentions/WebApplicationRegisteration.cs
@@ -0,0 +1,29 @@
+﻿using E_Commerce.Domain.Contracts;
+using E_Commerce.Persistence.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Web.Extentions
+{
+    public static class WebApplicationRegisteration
+    {
+        public static async Task<WebApplication> MigrateDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateAsyncScope();
+            var DbContextService = scope.ServiceProvider.GetRequiredService<StoreDbContext>();
+            var pendingMigrations = await DbContextService.Database.GetPendingMigrationsAsync();
+            if (pendingMigrations.Any())
+            {
+                await DbContextService.Database.MigrateAsync();
+            }
+            return app;
+        }
+
+        public static async Task<WebApplication> SeedDataAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateAsyncScope();
+            var DataIntializerService = scope.ServiceProvider.GetRequiredService<IDataIntializer>();
+            await DataIntializerService.IntializeAsync();
+            return app;
+        }
+    }
+}
